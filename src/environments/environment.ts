db083a1e22a162e13de7b41aa32eb8e1004c041b
luq-sham <luqman.shamsul10@gmail.com>
2025-06-10 09:55:26 +0800

    commit remove secrete

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,14 +4,7 @@
 
 export const environment = {
   production: false,
-  firebase: {
-    apiKey: 'AIzaSyBkqiT12zmNjS7CNgMkTiDkWwY1UlcOdTk',
-    authDomain: 'spenly-f71a2.firebaseapp.com',
-    projectId: 'spenly-f71a2',
-    storageBucket: 'spenly-f71a2.firebasestorage.app',
-    messagingSenderId: '313719504056',
-    appId: '1:313719504056:web:1916f1e1544869dfa4484d',
-  },
+  
 };
 /*
  * For easier debugging in development mode, you can import the following file
